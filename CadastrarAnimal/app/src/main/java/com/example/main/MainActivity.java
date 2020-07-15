b70d8b546b93d67dfc0f6710138688d5e7394edc
cristiellen <cristiellenalvess@gmail.com>
2020-07-14 21:43:06 -0400

    listar animais completo

--- CadastrarAnimal/app/src/main/java/com/example/main/MainActivity.java
+++ CadastrarAnimal/app/src/main/java/com/example/main/MainActivity.java
@@ -1,11 +1,18 @@
 package com.example.main;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ListView;
+
 import com.example.animal.activity.CadastrarAnimalActivity;
 import com.example.animal.dao.ListarAnimais;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
@@ -22,14 +29,31 @@ public class MainActivity extends AppCompatActivity {
 
         }
 
-
-        // TODO: PARA AMANHÃ IMPLEMENTAR LÓGICA DE LISTAR
         // TODO: IMPLEMENTAR CRIAÇÃO DA TABELA DO BEBEDOURO
         // TODO: IMPLEMENTAR CADASTRO DO BEBEDOURO
 
+        List<String> resultados = new ArrayList();
+        Cursor cursor =  new ListarAnimais().listar(this);
 
+        while (cursor.moveToNext()) {
+            // caminhando pelo conteúdo e atribuindo
+            String nome = cursor.getString(cursor.getColumnIndex("nome"));
+            int quantidade = Integer.parseInt(cursor.getString(cursor.getColumnIndex("quantidade")));
+            resultados.add( "Nome: "+ nome +" Quantidade: " + quantidade );
+        }
+
+
+        ListView listaView = (ListView) findViewById(R.id.lista);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, resultados );
+        listaView.setAdapter(adapter);
+        cursor.close();
 
     }
 
-
 }
+
+
+
+
+
+
